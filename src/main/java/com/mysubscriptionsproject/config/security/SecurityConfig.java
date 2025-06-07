56fb17d530600b8d5ab71ec74607c5da78bd8525
mtuhaud <47672475+mtuhaud@users.noreply.github.com>
2025-06-07 20:13:05 +0200

    csrf et état session

--- src/main/java/com/mysubscriptionsproject/config/security/SecurityConfig.java
+++ src/main/java/com/mysubscriptionsproject/config/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -21,13 +22,16 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(auth -> auth
-                        //.requestMatchers("/api/v1/users/**").permitAll()
+                .authorizeHttpRequests(auth ->
+                        auth
+                        .requestMatchers("/api/v1/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 //.formLogin(withDefaults()) // Formulaire de login par défaut
-                .httpBasic(withDefaults())
-                .logout(withDefaults());   // Gestion du logout
+                .httpBasic(withDefaults());
+                //.logout(withDefaults());   // Gestion du logout
         return http.build();
     }
 
